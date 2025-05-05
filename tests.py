5080e66d7a1dc53119c0eb678d748af22fe995ff
ninazhdanova1 <nina.zhdanova@icloud.com>
2025-05-05 01:01:13 -0400

    10 Tests for BooksCollector added
    10 Tests for BooksCollector added - tests.py
    
    description for tests added - README.md

--- tests.py
+++ tests.py
@@ -1,72 +1,70 @@
 import pytest
 from main import BooksCollector
 
-# Фикстура создаёт новый экземпляр BooksCollector для каждого теста
-@pytest.fixture
-def collector():
-    return BooksCollector()
+class TestBooksCollector:
 
-# Тест на добавление двух книг
-def test_add_new_book_add_two_books(collector):
-    collector.add_new_book('Гордость и предубеждение и зомби')
-    collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-    assert len(collector.get_books_genre()) == 2
+    def test_add_new_book_add_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 2
 
-# Параметризация: проверка некорректных названий
-@pytest.mark.parametrize('book_name', ['', 'А' * 41])
-def test_add_new_book_invalid_names_not_added(collector, book_name):
-    collector.add_new_book(book_name)
-    assert book_name not in collector.get_books_genre()
+    @pytest.mark.parametrize('name', ['', 'A' * 41])
+    def test_add_new_book_invalid_name_length(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name not in collector.get_books_genre()
 
-# Тест: повторное добавление одной и той же книги
-def test_add_new_book_duplicate(collector):
-    collector.add_new_book('Дюна')
-    collector.add_new_book('Дюна')
-    assert len(collector.get_books_genre()) == 1
+    def test_add_new_book_duplicate_not_added(self):
+        collector = BooksCollector()
+        collector.add_new_book('Оно')
+        collector.add_new_book('Оно')
+        assert len(collector.get_books_genre()) == 1
 
-# Тест: установка жанра книге
-def test_set_book_genre_valid(collector):
-    collector.add_new_book('1984')
-    collector.set_book_genre('1984', 'Фантастика')
-    assert collector.get_book_genre('1984') == 'Фантастика'
+    def test_set_book_genre_successfully(self):
+        collector = BooksCollector()
+        collector.add_new_book('Интерстеллар')
+        collector.set_book_genre('Интерстеллар', 'Фантастика')
+        assert collector.get_book_genre('Интерстеллар') == 'Фантастика'
 
-# Тест: у добавленной книги нет жанра по умолчанию
-def test_added_book_has_no_genre(collector):
-    collector.add_new_book('Без жанра')
-    assert collector.get_book_genre('Без жанра') == ''
+    def test_get_books_with_specific_genre_returns_correct_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Интерстеллар')
+        collector.set_book_genre('Интерстеллар', 'Фантастика')
+        collector.add_new_book('Паранормальное')
+        collector.set_book_genre('Паранормальное', 'Ужасы')
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Интерстеллар']
 
-# Тест: список книг с определённым жанром
-def test_get_books_with_specific_genre(collector):
-    collector.add_new_book('Шерлок')
-    collector.set_book_genre('Шерлок', 'Детективы')
-    assert collector.get_books_with_specific_genre('Детективы') == ['Шерлок']
+    def test_get_books_for_children_excludes_age_restricted_genres(self):
+        collector = BooksCollector()
+        collector.add_new_book('Маша и Медведь')
+        collector.set_book_genre('Маша и Медведь', 'Мультфильмы')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert 'Оно' not in collector.get_books_for_children()
+        assert 'Маша и Медведь' in collector.get_books_for_children()
 
-# Тест: книги с возрастным рейтингом отсутствуют в списке детских
-def test_get_books_for_children_excludes_age_restricted(collector):
-    collector.add_new_book('Оно')
-    collector.set_book_genre('Оно', 'Ужасы')
-    collector.add_new_book('Корпорация монстров')
-    collector.set_book_genre('Корпорация монстров', 'Мультфильмы')
-    result = collector.get_books_for_children()
-    assert 'Оно' not in result
-    assert 'Корпорация монстров' in result
+    def test_add_book_in_favorites_once(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер')
+        collector.add_book_in_favorites('Гарри Поттер')
+        collector.add_book_in_favorites('Гарри Поттер')
+        assert collector.get_list_of_favorites_books() == ['Гарри Поттер']
 
-# Тест: добавление книги в избранное
-def test_add_book_in_favorites(collector):
-    collector.add_new_book('Хоббит')
-    collector.add_book_in_favorites('Хоббит')
-    assert collector.get_list_of_favorites_books() == ['Хоббит']
+    def test_delete_book_from_favorites_removes_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('1984')
+        collector.add_book_in_favorites('1984')
+        collector.delete_book_from_favorites('1984')
+        assert collector.get_list_of_favorites_books() == []
 
-# Тест: книга не добавляется повторно в избранное
-def test_add_book_in_favorites_twice(collector):
-    collector.add_new_book('Хоббит')
-    collector.add_book_in_favorites('Хоббит')
-    collector.add_book_in_favorites('Хоббит')
-    assert collector.get_list_of_favorites_books() == ['Хоббит']
+    def test_get_book_genre_returns_none_if_not_set(self):
+        collector = BooksCollector()
+        collector.add_new_book('Книга без жанра')
+        assert collector.get_book_genre('Книга без жанра') == ''
 
-# Тест: удаление книги из избранного
-def test_delete_book_from_favorites(collector):
-    collector.add_new_book('Хоббит')
-    collector.add_book_in_favorites('Хоббит')
-    collector.delete_book_from_favorites('Хоббит')
-    assert collector.get_list_of_favorites_books() == []
+    def test_get_books_genre_returns_correct_dictionary(self):
+        collector = BooksCollector()
+        collector.add_new_book('Матрица')
+        collector.set_book_genre('Матрица', 'Фантастика')
+        assert collector.get_books_genre() == {'Матрица': 'Фантастика'}
